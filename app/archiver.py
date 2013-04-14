1c05f2ee3c62664d17bc74933b7f1645faf5bbec
unknown <jeffrimko@gmail.com>
2013-04-14 10:11:37 -0400

    Work in progress. Updating the basic application logic.

--- app/archiver.py
+++ app/archiver.py
@@ -2,11 +2,11 @@
 
 An archive is simply a zip file containing a log file plus
 any additional files to archive. The standard used for the
-log file is a text file named '__archive_info__.txt'.
+log file is a text file named `__archive_info__.txt`.
 The log is always in the root directory of the archive.
 
 Usage:
-  archiver  TARGET... [-m LOGMSG] [-n NAME]
+  archiver [options] TARGET...
   archiver -h | --help
   archiver --version
 
@@ -14,10 +14,14 @@ Arguments:
   TARGET   Path to a file or folder to archive.
 
 Options:
-  -m LOGMSG  Archive log message.
-  -n NAME    Archive name.
-  -h --help  Show this help message and exit.
-  --version  Show version and exit.
+  -m LOGMSG         Archive log message.
+  --outdir=OUTDIR   Directory to place generated files [default: .].
+  --name=NAME       Archive name.
+  --no_name_ts      Do not include timestamp in archive name.
+  --short_ts        Only timestamp to the day (min:sec otherwise).
+  --delete          Delete original targets from file system after archiving.
+  -h --help         Show this help message and exit.
+  --version         Show version and exit.
 """
 
 ##==============================================================#
@@ -28,7 +32,11 @@ Options:
 ## SECTION: Imports                                             #
 ##==============================================================#
 
-import os, shutil, sys, time
+import os
+import shutil
+import tempfile
+import sys
+import time
 from zipfile import ZipFile, ZIP_DEFLATED
 
 from docopt import docopt
@@ -47,16 +55,17 @@ __version__ = "1.0.0"
 class UtilData(dict):
     """Object used to organize data within the utility."""
     def __init__(self):
-        self['files'] = []     # Path of files to archive.
+        self['targets'] = []   # Path of targets to archive.
         self['log_text'] = ""  # Text for log.
         self['name'] = ""      # Name of the archive.
+        self['outdir'] = "."   # Output directory for the archive.
 
-        # True if a timestamp should be added to the archive name.
-        self['add_timestamp'] = True
-        # True if the original files should be deleted after archiving.
-        self['del_originals'] = False
-        # True if the timestamp should include time down to the second, otherwise to the day.
-        self['precise_time'] = True
+        # False if a timestamp should not be added to the archive name.
+        self['no_name_ts'] = False
+        # True if the original targets should be deleted from the file system after archiving.
+        self['delete'] = False
+        # False if the timestamp should only go to the day (min:sec otherwise).
+        self['short_ts'] = True
 
 ##==============================================================#
 ## SECTION: Function Definitions                                #
@@ -81,6 +90,74 @@ def _get_base_files(root_dir, base_targets):
                 files.append(os.path.join(target, file))
     return files
 
+def get_files(rootdir):
+    """Returns a list of files contained in the given root directory and all
+    subdirectories. List is returned as absolute paths of fies.
+    """
+    files = []
+    for i in os.listdir(rootdir):
+        ipath = os.path.join(rootdir, i)
+        if os.path.isdir(ipath):
+            files += get_files(ipath)
+        elif os.path.isfile(ipath):
+            files.append(ipath)
+    return files
+
+def zip_targets(zippath, targets, top_targets=[]):
+    """Creates a zip file containing the given target files or directories."""
+    # Gather the list of files to zip; expand directories.
+    files = []
+    for t in targets:
+        t = os.path.abspath(t)
+        if os.path.isdir(t):
+            files += get_files(t)
+        elif os.path.isfile(t):
+            files.append(t)
+        else:
+            sys.exit("ERROR: Could not locate '%s'!" % (t))
+
+    # Determine the zipfile name for each file.
+    # Zipfile names are determined by removing the common prefix.
+    zfiles = []
+    cpf = os.path.commonprefix(files)
+    for t in files:
+        zfiles.append(os.path.splitdrive(t[len(cpf):])[1])
+
+    # Create zip archive and write files.
+    z = zipfile.ZipFile(zippath, "w")
+    for a,b in zip(files, zfiles):
+        z.write(a,b)
+
+def get_timestamps(short_ts=False):
+    """Get the current timestamp to be applied to the archive and the log."""
+    if not short_ts:
+        archive_ts = time.strftime("%Y%m%d%H%M")
+        log_ts = time.strftime("%d %B %Y, ")
+        log_ts += ("%s") % (time.strftime("%I:%M%p (%Z)")).lstrip('0')
+    else:
+        archive_ts = time.strftime("%Y%m%d")
+        log_ts = time.strftime("%d %B %Y")
+    return archive_ts, log_ts
+
+def create_logfile(name, txt, ts=None):
+    tmpdir = tempfile.mkdtemp()
+    logpath = os.path.join(tmpdir, "__archive_info__.txt")
+
+    # Create the log file.
+    logfile = open(logpath, 'w')
+    logfile.write(name + '\n')
+    for _ in range(len(name)):
+        logfile.write("=")
+    logfile.write("\n")
+    logfile.write(":date: " + ts + '\n\n')
+
+    # Write text to file.
+    for l in txt:
+        logfile.write(l)
+    logfile.write("\n")
+    logfile.close()
+    return logpath
+
 def create_archive(udata):
     """This function creates a new archive zip file. The archive file
     will be placed in the parent directory that is common to all target
@@ -91,132 +168,36 @@ def create_archive(udata):
     `python archiver.py  my_fileA.txt  my_fileB.doc` on 2 January 2010
     at 1:30pm will result in a zip archive named
     '201001021330-my_fileA.zip'.
-
-    Parameters:
-     - targets - Paths to the files and folders to add to the archive.
-     - log_text - The text that will be written to the archive log.
-     - name - The name of the archive. If an empty string, the default name will be used.
-     - add_timestamp - True if a timestamp should be added to the archive name.
-     - del_originals - True if the original files should be deleted after archiving.
-     - precise_time - True if the timestamp should include time down to the second, otherwise to the day.
     """
     if not udata['targets']:
         sys.exit("ERROR: Must provide at least one target!")
+    archive_ts, log_ts = get_timestamps(short_ts=udata['short_ts'])
 
-    for t in udata['targets']:
-        if (not os.path.isdir(t)) and (not os.path.isfile(t)):
-            sys.exit("ERROR: Could not locate '%s'!" % (t))
+    logpath = create_logfile(udata['name'], udata['log_text'])
 
-    print "OK"
-    exit()
-
-    # Get the current time-stamp to be applied to the archive and the log.
-    if precise_time:
-        archive_time_stamp = time.strftime("%Y%m%d%H%M")
-        log_time_stamp = time.strftime("%d %B %Y, ")
-        log_time_stamp += ("%s") % (time.strftime("%I:%M%p (%Z)")).lstrip('0')
-    else:
-        archive_time_stamp = time.strftime("%Y%m%d")
-        log_time_stamp = time.strftime("%d %B %Y")
-
-    # Get the archive root directory.
-    dir = os.path.dirname(targets[0])
-
-    # If the archive name has not been explicitly defined, generate one from the
-    # first target file name and the current timestamp.
-    if not name:
-        archive_name = os.path.splitext(os.path.basename(targets[0]))[0]
-    else:
-        archive_name = name
-
-    # Determine the path of the log file for creation.
-    log_name = "__archive_info__.txt"
-    log_path = os.path.join(dir, log_name)
-
-    # If a file exists that would conflict in name with the log, the log file is not created.
-    if not os.path.exists(log_path):
-
-        # Create the log file.
-        log_file = open(log_path, 'w')
-        log_file.write(archive_name + '\n')
-        for _ in range(len(archive_name)):
-            log_file.write("=")
-        log_file.write("\n")
-        log_file.write(":date: " + log_time_stamp + '\n\n')
-
-        # Column correct the text of the log.
-        for log_line in log_text:
-            log_file.write(log_line)
-        log_file.write("\n")
-        log_file.close()
-
-        # Add the log to the target files.
-        targets.insert(0, log_path)
-    else:
-        status = "Cannot create log file due to name conflict."
-
-    # Expand the relative paths of the target files and folder.
-    base_targets = []
-    for target in targets:
-        base_targets.append(os.path.basename(target))
-    files = _get_base_files(dir, base_targets)
-
-    # Create the zip file for the archive.
-    zip_name = archive_name + ".zip"
-    if add_timestamp:
-        zip_name = archive_time_stamp + '-' + zip_name
-    zip_file = ZipFile(os.path.join(dir, zip_name), 'w', compression=ZIP_DEFLATED)
-
-    # Add the files to the archive.
-    for file in files:
-        zip_file.write(os.path.join(dir, file), file)
-
-    # Delete original files if the option is selected.
-    # Otherwise, delete only the log file.
-    try:
-        if del_originals:
-            # Delete files first.
-            for file in files:
-                os.remove(os.path.join(dir, file))
-
-            # Delete folders once files are gone.
-            for target in targets:
-                if os.path.exists(target) and os.path.isdir(target):
-                        shutil.rmtree(target)
-        else:
-            os.remove(log_path)
-    except:
-        status = str(sys.exc_info()[1])
+    print zippath
     return status
 
 def parse_args(args):
-    print args
     udata = UtilData()
-    udata['targets'] = args['TARGET']
+    udata['targets'] = [os.path.abspath(t) for t in args['TARGET']]
+    udata['name'] = args['--name']
     udata['log_text'] = args['-m']
-    print udata
+    udata['short_ts'] = args['--short_ts']
+    udata['no_name_ts'] = args['--no_name_ts']
+    udata['delete'] = args['--delete']
     return udata
 
 def main():
     args = docopt(__doc__, version=__version__)
     udata = parse_args(args)
-    create_archive(udata)
 
-    if len(sys.argv) > 1:
-        if "--version" == sys.argv[1]:
-            print "archiver %s" % (__version__)
-            sys.exit()
-        elif "--help" == sys.argv[1]:
-            print USAGE
-            sys.exit()
-        elif "-h" == sys.argv[1]:
-            print USAGE
-            sys.exit()
-        # log_text = raw_input("Enter log contents: ")
-        log_text = "BLAH"
-        create_archive(sys.argv[1:], log_text)
-    else:
-        print USAGE
+    # If the archive name has not been explicitly defined, generate one from the
+    # first target file name and the current timestamp.
+    if not udata['name']:
+        udata['name'] = os.path.splitext(os.path.basename(udata['targets'][0]))[0]
+
+    create_archive(udata)
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
