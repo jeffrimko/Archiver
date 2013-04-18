45d3e1b4ae8c6bf23be9a95c1f72da0d7a0af131
unknown <jeffrimko@gmail.com>
2013-04-17 22:10:47 -0400

    Updated utility logic; all functions appear to be functional for the command line util. Still need to clean and document code.

--- app/archiver.py
+++ app/archiver.py
@@ -17,9 +17,11 @@ Options:
   -m LOGMSG         Archive log message.
   --outdir=OUTDIR   Directory to place generated files [default: .].
   --name=NAME       Archive name.
-  --no_name_ts      Do not include timestamp in archive name.
-  --short_ts        Only timestamp to the day (min:sec otherwise).
+  --no_timestamp    Do not include timestamp in archive name.
+  --short_timestamp Only timestamp to the day (hour:min otherwise).
+  --long_timestamp  Timestamp to the second (hour:min otherwise).
   --delete          Delete original targets from file system after archiving.
+  --flatten         Flatten directory structure in the zip archive.
   -h --help         Show this help message and exit.
   --version         Show version and exit.
 """
@@ -37,6 +39,7 @@ import shutil
 import tempfile
 import sys
 import time
+import zipfile
 from zipfile import ZipFile, ZIP_DEFLATED
 
 from docopt import docopt
@@ -61,34 +64,19 @@ class UtilData(dict):
         self['outdir'] = "."   # Output directory for the archive.
 
         # False if a timestamp should not be added to the archive name.
-        self['no_name_ts'] = False
+        self['no_ts'] = False
         # True if the original targets should be deleted from the file system after archiving.
         self['delete'] = False
-        # False if the timestamp should only go to the day (min:sec otherwise).
-        self['short_ts'] = True
+        # True if the timestamp should only go to the day (hour:min otherwise).
+        self['short_ts'] = False
+        # True if the timestamp should go to the second (min:sec otherwise).
+        self['long_ts'] = False
+        # True if the directory structure should be flattened.
+        self['flatten'] = False
 
 ##==============================================================#
 ## SECTION: Function Definitions                                #
 ##==============================================================#
-
-def _get_base_files(root_dir, base_targets):
-    """Returns an expanded list of files starting from the root
-    directory.
-
-    Parameters:
-     - root_dir - The absolute path of the root directory.
-     - base_targets - A list of files or folders that exist in
-       the root directory. Directories will be expanded.
-    """
-    files = []
-    for target in base_targets:
-        if os.path.isfile(os.path.join(root_dir, target)):
-            files.append(target)
-        elif os.path.isdir(os.path.join(root_dir, target)):
-            dir_files = _get_base_files(os.path.join(root_dir, target), os.listdir(os.path.join(root_dir, target)))
-            for file in dir_files:
-                files.append(os.path.join(target, file))
-    return files
 
 def get_files(rootdir):
     """Returns a list of files contained in the given root directory and all
@@ -103,7 +91,7 @@ def get_files(rootdir):
             files.append(ipath)
     return files
 
-def zip_targets(zippath, targets, top_targets=[]):
+def zip_targets(zippath, targets=[], top_files=[], flatten=False, delete=False):
     """Creates a zip file containing the given target files or directories."""
     # Gather the list of files to zip; expand directories.
     files = []
@@ -119,44 +107,27 @@ def zip_targets(zippath, targets, top_targets=[]):
     # Determine the zipfile name for each file.
     # Zipfile names are determined by removing the common prefix.
     zfiles = []
-    cpf = os.path.commonprefix(files)
-    for t in files:
-        zfiles.append(os.path.splitdrive(t[len(cpf):])[1])
+    cpf = os.path.dirname(os.path.commonprefix(files))
+    for f in files:
+        zfiles.append(os.path.splitdrive(f[len(cpf):])[1])
+
+    # Add top files to lists; top files will always be at the root of the zip archive.
+    for f in top_files:
+        f = os.path.abspath(f)
+        if os.path.isfile(f):
+            f = os.path.abspath(f)
+            files.append(f)
+            zfiles.append(os.path.basename(f))
 
     # Create zip archive and write files.
-    z = zipfile.ZipFile(zippath, "w")
-    for a,b in zip(files, zfiles):
-        z.write(a,b)
-
-def get_timestamps(short_ts=False):
-    """Get the current timestamp to be applied to the archive and the log."""
-    if not short_ts:
-        archive_ts = time.strftime("%Y%m%d%H%M")
-        log_ts = time.strftime("%d %B %Y, ")
-        log_ts += ("%s") % (time.strftime("%I:%M%p (%Z)")).lstrip('0')
-    else:
-        archive_ts = time.strftime("%Y%m%d")
-        log_ts = time.strftime("%d %B %Y")
-    return archive_ts, log_ts
-
-def create_logfile(name, txt, ts=None):
-    tmpdir = tempfile.mkdtemp()
-    logpath = os.path.join(tmpdir, "__archive_info__.txt")
-
-    # Create the log file.
-    logfile = open(logpath, 'w')
-    logfile.write(name + '\n')
-    for _ in range(len(name)):
-        logfile.write("=")
-    logfile.write("\n")
-    logfile.write(":date: " + ts + '\n\n')
-
-    # Write text to file.
-    for l in txt:
-        logfile.write(l)
-    logfile.write("\n")
-    logfile.close()
-    return logpath
+    archive = zipfile.ZipFile(zippath, "w")
+    for f,z in zip(files, zfiles):
+        if not flatten:
+            archive.write(f, z)
+        else:
+            archive.write(f, os.path.basename(z))
+        if delete:
+            os.remove(f)
 
 def create_archive(udata):
     """This function creates a new archive zip file. The archive file
@@ -171,32 +142,96 @@ def create_archive(udata):
     """
     if not udata['targets']:
         sys.exit("ERROR: Must provide at least one target!")
-    archive_ts, log_ts = get_timestamps(short_ts=udata['short_ts'])
-
-    logpath = create_logfile(udata['name'], udata['log_text'])
-
-    print zippath
-    return status
-
-def parse_args(args):
-    udata = UtilData()
-    udata['targets'] = [os.path.abspath(t) for t in args['TARGET']]
-    udata['name'] = args['--name']
-    udata['log_text'] = args['-m']
-    udata['short_ts'] = args['--short_ts']
-    udata['no_name_ts'] = args['--no_name_ts']
-    udata['delete'] = args['--delete']
-    return udata
-
-def main():
-    args = docopt(__doc__, version=__version__)
-    udata = parse_args(args)
 
     # If the archive name has not been explicitly defined, generate one from the
     # first target file name and the current timestamp.
     if not udata['name']:
         udata['name'] = os.path.splitext(os.path.basename(udata['targets'][0]))[0]
+    name = udata['name']
+    name += ".zip"
 
+    # Add timestamp to archive name.
+    if not udata['no_ts']:
+        if udata['long_ts']:
+            name = "%s-%s" % (time.strftime("%Y%m%d%H%M%S"), name)
+        elif udata['short_ts']:
+            name = "%s-%s" % (time.strftime("%Y%m%d"), name)
+        else:
+            name = "%s-%s" % (time.strftime("%Y%m%d%H%M"), name)
+
+    # Check that file with same name doesn't already exist.
+    if os.path.isfile(name):
+        sys.exit("ERROR: Archive named '%s' already exists!" % (name))
+
+    # Create temporary directory.
+    tmpdir = tempfile.mkdtemp()
+
+    # Create archive info file.
+    top_files = []
+    if udata['log_text']:
+        logpath = os.path.join(tmpdir, "__archive_info__.txt")
+        if not create_notefile(logpath, udata['name'], udata['log_text']):
+            shutil.rmtree(tmpdir)
+            sys.exit("ERROR: Could not create archive info file!")
+        top_files.append(logpath)
+
+    # Create archive and zip targets.
+    zip_targets(name, udata['targets'], top_files, udata['flatten'], udata['delete'])
+
+    # Remove temporary directory.
+    shutil.rmtree(tmpdir)
+
+def create_notefile(path, title, txt="", attrs={}):
+    """Creates an Asciidoc note file.
+
+    **Parameters**:
+      - path (str) - Path of the file to create.
+      - title (str) - Title of the note file.
+      - txt (str) - Text content of the note file.
+      - attrs (dict) - Additional attributes to add to the file;
+        by default only the `:date:` attribute is created.
+    """
+    # Generate timestamp.
+    ts = time.strftime("%I:%M%p (%Z)").lstrip('0')
+
+    # Check that path directory exists.
+    path = os.path.abspath(path)
+    if not os.path.isdir(os.path.dirname(path)):
+        return None
+
+    # Create the log file; write title and metadata.
+    logfile = open(path, 'w')
+    logfile.write(title + '\n')
+    for _ in range(len(title)):
+        logfile.write("=")
+    logfile.write("\n")
+    logfile.write(":date: " + ts + '\n')
+    for key in attrs:
+        logfile.write(":%s: %s\n" % (key, attrs[key]))
+    logfile.write("\n")
+
+    # Write text to file.
+    logfile.write(txt)
+    logfile.write("\n")
+    logfile.close()
+    return path
+
+def parse_args(args):
+    """Parses command line arguments into a UtilData object."""
+    udata = UtilData()
+    udata['targets'] = [os.path.abspath(t) for t in args['TARGET']]
+    udata['name'] = args['--name']
+    udata['log_text'] = args['-m']
+    udata['short_ts'] = args['--short_timestamp']
+    udata['long_ts'] = args['--long_timestamp']
+    udata['no_ts'] = args['--no_timestamp']
+    udata['delete'] = args['--delete']
+    udata['flatten'] = args['--flatten']
+    return udata
+
+def main():
+    args = docopt(__doc__, version=__version__)
+    udata = parse_args(args)
     create_archive(udata)
 
 ##==============================================================#
