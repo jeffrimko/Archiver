2eed7bbc95d876a713b1278ff2d9678b095a0578
jeffrimko <jeffrimko@gmail.com>
2018-02-07 21:07:55 -0500

    Added helper scripts.

---
+++ _Check_Versions.py
@@ -0,0 +1,38 @@
+##==============================================================#
+## SECTION: Imports                                             #
+##==============================================================#
+
+import qprompt
+import verace
+
+##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+CLI = verace.VerChecker("Archiver", __file__)
+CLI.include(r"app\appinfo.py", match="ARCHIVER_VER = ", splits=[('"',1)])
+CLI.include(r"CHANGELOG.md", match=" archiver-", splits=[("-",1),(" ",0)], updatable=False)
+
+GUI = verace.VerChecker("gArchiver", __file__)
+GUI.include(r"app\appinfo.py", match="GARCHIVER_VER = ", splits=[('"',1)])
+GUI.include(r"CHANGELOG.md", match=" garchiver-", splits=[("-",1),(" ",0)], updatable=False)
+
+##==============================================================#
+## SECTION: Function Definitions                                #
+##==============================================================#
+
+def show():
+    print("CLI = " + (CLI.string() or "mismatch!"))
+    print("GUI = " + (GUI.string() or "mismatch!"))
+
+##==============================================================#
+## SECTION: Main Body                                           #
+##==============================================================#
+
+if __name__ == '__main__':
+    show()
+    menu = qprompt.Menu()
+    menu.add("c", "CLI version", CLI.prompt)
+    menu.add("g", "GUI version", GUI.prompt)
+    menu.add("s", "show all versions", show)
+    menu.main(loop=True)
